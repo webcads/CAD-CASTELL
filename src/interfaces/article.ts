6843b0110edc85f7cbaaeaa7a04e580ad66ce405
castelll0009 <castelll0009@gmail.com>
2023-12-17 12:27:42 -0500

    Update article.ts
    similar  to tour just  with title and description

---
+++ src/interfaces/article.ts
@@ -0,0 +1,20 @@
+// export default interface Article {
+//     id: number;
+//     attributes: {
+//       title: string;
+//       description: string;
+//       content: string;
+//       slug: string;
+//       createdAt: string;
+//       updatedAt: string;
+//       publishedAt: string;
+//     };
+//   }
+
+export default interface Article {
+      id: number;
+      attributes: {
+        title: string;
+        description: string;        
+      };
+    }
