21b066a0c8b3f354432b7abd372b968efa3aad2b
castelll0009 <castelll0009@gmail.com>
2023-12-16 20:38:28 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  ssr: false,
+  integrations: [react(), tailwind()],
+  site: 'https://castelll0009.github.io',
+  base: '/CAD'
+});
