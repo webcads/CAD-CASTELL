02b2dc911ccd68d6c534dda3d45a81b8f970ecd4
castelll0009 <castelll0009@gmail.com>
2023-12-25 11:26:51 -0500

    Branfing Component
    and color whit dark ligth functionality

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				body: "gray",				
+				silver:"silver",
+				secondary: "#2b2c30",
+				secondary_dark: "#202020",
+				brandingColor: "cyan",
+				third:"#6ae8aa"
+			  }
+		},
 	},
 	plugins: [],
 }
