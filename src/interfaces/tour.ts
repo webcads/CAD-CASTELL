9a4589cc1374939d7439d6f1bf87070fafd98098
castelll0009 <castelll0009@gmail.com>
2023-12-28 13:59:31 -0500

    strapi markdown . basic fecth ready basi

---
+++ src/interfaces/tour.ts
@@ -0,0 +1,25 @@
+// export default interface Article {
+//     id: number;
+//     attributes: {
+//       title: string;
+//       description: string;
+//       content: string;
+//       slug: string;
+//       createdAt: string;
+//       updatedAt: string;
+//       publishedAt: string;
+//     };
+//   }
+
+export default interface Tour {
+    id: number;
+    attributes: {
+      title: string;
+      description: string;
+      content: string;
+      slug: string;
+      createdAt: string;
+      updatedAt: string;
+      publishedAt: string;
+    };
+  }
