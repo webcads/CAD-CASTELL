fca4c3c3f9dae64c8fa9b3d06855cd0b13f7d497
castelll0009 <castelll0009@gmail.com>
2023-12-29 01:21:41 -0500

    mapping from strapi data base
    the enviroment is able to deploy images from strapi data base

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -1,25 +1,25 @@
-// export default interface Article {
-//     id: number;
-//     attributes: {
-//       title: string;
-//       description: string;
-//       content: string;
-//       slug: string;
-//       createdAt: string;
-//       updatedAt: string;
-//       publishedAt: string;
-//     };
-//   }
+// interfaces/article.ts
 
 export default interface Article {
-    id: number;
-    attributes: {
-      title: string;
-      description: string;
-      content: string;
-      slug: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
+  id: number;
+  attributes: {
+    title: string;
+    description: string;
+    content: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    image: {
+      data: {
+        id: number;
+        attributes: {
+          name: string;
+          alternativeText: string | null;
+          url: string;
+          // Add other necessary image properties here
+        };
+      } | null;
     };
-  }
+  };
+}
