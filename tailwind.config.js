9623c0349515f03adee915e146f97f14a1379490
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-01-17 03:36:14 -0500

    asved last changes

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [  "./node_modules/flowbite/**/*.js" , "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  // plugins: [  require('flowbite/plugin')],
+}
+
